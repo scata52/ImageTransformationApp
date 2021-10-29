c54efa6adf97848cba5c8b74e4091420a3310757
Cem Atalay <Cem.Atalay@campus.lmu.de>
2021-10-29 09:49:10 +0200

    added advanced view

--- src/main/java/com/example/demo/ImageTransformationController.java
+++ src/main/java/com/example/demo/ImageTransformationController.java
@@ -3,20 +3,47 @@ package com.example.demo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class ImageTransformationController {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    /*@FXML
+    private Button chooseInputFileBTN;
+
+    @FXML
+    private Button chooseOutputFileBTN;
+
+    @FXML
+    private Button setParametersBTN;*/
+
+   /* @FXML
+    private ChoiceBox<String> choiceBox;
+
+    private final String[] transformationTypes = {"Gabor Transformation"};*/
+
+    @FXML
+    private ListView<String> inputFilePath;
+
+    @FXML
+    private ListView<String> outputFilePath;
 
     public void switchToStartView(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(ImageTransformation.class.getResource("start_view.fxml")));
@@ -25,7 +52,6 @@ public class ImageTransformationController {
         stage.setScene(scene);
         stage.show();
     }
-
 
     public void switchToBasicView(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(ImageTransformation.class.getResource("basic_view.fxml")));
@@ -41,6 +67,28 @@ public class ImageTransformationController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void chooseInputFiles(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            inputFilePath.getItems().add(selectedFile.getAbsolutePath());
+        } else {
+            System.out.println("File not valid!");
+        }
+    }
+
+    public void chooseOutputFiles(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            outputFilePath.getItems().add(selectedFile.getAbsolutePath());
+        } else {
+            System.out.println("File not valid!");
+        }
     }
 
 }
